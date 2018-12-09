9ca988b2d551698b4c846461da4edafead0ae7d3
camron <camronmoradi@gmail.com>
2018-12-09 11:20:22 +0100

    Adds tailwind, updates spec file and functions, creates view template, creates contact component

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,17 @@ export class AppPage {
   getTitleText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getField(name) {
+    return element(by.name(name));  // will locate a element by its name and return it
+  }
+
+  getSubmitButton() {
+    return element(by.buttonText('Submit'));  // will return a button element that has text submit
+  }
+
+  cardCount() {
+    return element.all(by.css('.card')).count();  // finds all elements with card class and returns how many they are
+  }
+
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'address-book';
+  contact: any
+  contacts: any[] = [];
+
+  constructor() {
+    this.initContact();
+  }
+
+  createNewContact() {
+    console.log(`Creating the following contact: ${JSON.stringify(this.contact)}`);
+    this.contacts.push(this.contact);
+    this.initContact();
+  }
+
+  private initContact() {
+    this.contact = {
+      name: ``,
+      email: ``,
+      company: ``,
+      role: ``,
+      twitter: ``,
+      location: ``,
+      notes: ``
+    }
+  }
 }
